6fdb35af10e497e5761065733ec724a125a5e927
Uday Annapureddy <uday61543@gmail.com>
2024-06-17 18:46:29 +0530

    filter added to client

--- src/main/java/com/jsp/warehouse/repo/ClientRepository.java
+++ src/main/java/com/jsp/warehouse/repo/ClientRepository.java
@@ -1,10 +1,14 @@
 package com.jsp.warehouse.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jsp.warehouse.models.Client;
 
 
 public interface ClientRepository extends JpaRepository<Client, Integer>{
+
+	Optional<Client> findByEmail(String email);
 
 }
